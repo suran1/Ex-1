2c7c6385cf43030e1def15b5528cf847caa7dd9b
Suranjita Timilsina <er.suran@gmail.com>
2016-10-10 19:25:12 -0500

    add newRepo

---
+++ exmath.js
@@ -0,0 +1,11 @@
+var firstNumber = 4;
+var secondNumber = 7;
+var sum = firstNumber +secondNumber;
+console.log('the sum is : ', sum);
+
+
+
+var thirdNumber = 8;
+var fourthNumber = 5;
+var sub = thirdNumber + fourthNumber;
+console.log('sub is : ',sub);
